828fd4c18b8fd8573c077d4d17468fc422ccaafd
markerstone <hugo.miguel.abreu@gmail.com>
2018-04-15 11:03:01 +0100

    Migrated torrent server to an object. Main only receives commands.

--- Server/src/main/java/ServerExec.java
+++ Server/src/main/java/ServerExec.java
@@ -1,8 +1,10 @@
 import Handlers.TorrentServerInitializer;
 import com.turn.ttorrent.client.SharedTorrent;
 import com.turn.ttorrent.common.Torrent;
+import com.turn.ttorrent.tracker.TrackedTorrent;
 import com.turn.ttorrent.tracker.Tracker;
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
@@ -11,50 +13,28 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ServerExec {
 
-    static final int PORT = Integer.parseInt(System.getProperty("port", "5000"));
-    static List<Torrent> trackedTorrents;
+    public static void main(String[] args) throws IOException, InterruptedException {
+        //Starts tracker;
+        Tracker tck = new Tracker(new InetSocketAddress(6969));
+        System.out.println("Tracker initiated");
+        MainServer ms = new MainServer(5000, tck);
+        //Starts server;
+        ms.start();
 
-    public ServerExec(){
-        trackedTorrents = new ArrayList<>();
-    }
-
-    public static void main(String[] args) {
-        trackedTorrents = new ArrayList<>();
-        //Acceptor
-        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
-        //Workers
-        EventLoopGroup workerGroup = new NioEventLoopGroup();
-
-        try {
-            ServerBootstrap b = new ServerBootstrap();
-            b.group(bossGroup, workerGroup)
-                .channel(NioServerSocketChannel.class)
-                .childHandler(new TorrentServerInitializer(trackedTorrents));
-
-            b.bind(PORT).sync().channel().closeFuture();
-            System.out.println("Server initiated");
-            Tracker tck = new Tracker(new InetSocketAddress(6969));
-            System.out.println("Tracker initiated");
-
-            while (true){
-                if(!trackedTorrents.isEmpty()){
-                    for (Torrent t:trackedTorrents) {
-                        System.out.println(t.getName());
-                        System.out.println(t.getCreatedBy());
-                        System.out.println("---------------------------");
-                    }
-                }
-                Thread.sleep(5000);
+        Scanner reader = new Scanner(System.in);
+        String line;
+        while (!(line = reader.nextLine()).equals("quit")){
+            for (TrackedTorrent t:tck.getTrackedTorrents()) {
+                System.out.println(t.getName());
+                System.out.println(t.getCreatedBy());
+                System.out.println("---------------------------");
             }
-        } catch (InterruptedException | IOException e) {
-            e.printStackTrace();
-        } finally {
-            bossGroup.shutdownGracefully();
-            workerGroup.shutdownGracefully();
         }
+        ms.shutdown();
     }
 
 }
