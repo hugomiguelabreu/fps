250f6831653d4a974f26e760bed95961f04ba7ed
markerstone <hugo.miguel.abreu@gmail.com>
2018-04-15 11:06:10 +0100

    Changed creator of torrents to return only bytes. Changed main to send torrents via netty.

--- Client/src/main/java/Main.java
+++ Client/src/main/java/Main.java
@@ -1,19 +1,184 @@
+import Handlers.TorrentClientHandler;
+import Handlers.TorrentClientInitializer;
 import Misc.TorrentUtil;
+import Network.TorrentWrapperOuterClass;
+import com.google.protobuf.ByteString;
+import com.turn.ttorrent.client.Client;
+import com.turn.ttorrent.client.SharedTorrent;
+import com.turn.ttorrent.client.peer.SharingPeer;
 import com.turn.ttorrent.common.Torrent;
+import com.turn.ttorrent.tracker.TrackedTorrent;
+import com.turn.ttorrent.tracker.Tracker;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.*;
+import java.nio.channels.UnsupportedAddressTypeException;
 import java.security.NoSuchAlgorithmException;
+import java.util.*;
 
 public class Main {
 
+    private static final Logger logger = LoggerFactory.getLogger(Object.class);
+
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
 
-        TorrentUtil tu = new TorrentUtil("kk", "kk");
-        Torrent t = tu.createTorrent("file.torrent", "file.out", 512);
-        System.out.println(t.getName());
-        System.out.println(t.getSize());
-        System.out.println(t.isMultifile());
-        System.out.println(t.getCreatedBy());
+        Scanner sc = new Scanner(System.in);
+        String input;
+        String username = "default";
+        Torrent t = null;
+
+        Channel ch = startClient();
+        System.out.println("Started client");
+
+        while (!(input = sc.nextLine()).equals("quit")){
+            if(input.equals("login")){
+                username = sc.nextLine();
+            }
+            if(input.equals("upload")){
+                System.out.println("What is the file?");
+                ArrayList<String> trc = new ArrayList<String>();
+                trc.add("http://localhost:6969/annouce");
+                trc.add("http://localhost:8989/annouce");
+                t = TorrentUtil.createTorrent(sc.nextLine(), username, trc);
+
+                TorrentWrapperOuterClass.TorrentWrapper tw = TorrentWrapperOuterClass.TorrentWrapper.newBuilder().setContent(ByteString.copyFrom(t.getEncoded())).build();
+                try {
+                    //Escreve e espera pela escrita no socket
+                    ch.writeAndFlush(tw).sync();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Upload intention initiated");
+            }
+
+            if(input.equals("download")){
+                System.out.println("What is the file?");
+                String file = sc.nextLine();
+                try {
+                    final SharedTorrent st = SharedTorrent.fromFile(
+                            new File(file),
+                            new File("/tmp/" + sc.nextLine()));
+
+                    Client c = new Client(
+                            getIPv4Address(null),
+                            st);
+
+                    c.setMaxDownloadRate(0.0);
+                    c.setMaxUploadRate(0.0);
+
+                    //Download and seed
+                    c.addObserver(new Observer() {
+                        @Override
+                        public void update(Observable o, Object arg) {
+                            System.out.println(st.getCompletion());
+                            System.out.println(arg);
+                        }
+                    });
+                    c.share(-1);
+
+                    if (Client.ClientState.ERROR.equals(c.getState())) {
+                        System.exit(1);
+                    }
+                } catch (Exception e) {
+                    logger.error("Fatal error: {}", e.getMessage(), e);
+                    System.exit(2);
+                }
+            }
+
+            if(input.equals("track")) {
+                FilenameFilter filter = new FilenameFilter() {
+                    @Override
+                    public boolean accept(File dir, String name) {
+                        return name.endsWith(".torrent");
+                    }
+                };
+
+                try {
+                    Tracker tck = new Tracker(new InetSocketAddress(6969));
+
+                    File parent = new File("./");
+                    for (File f : parent.listFiles(filter)) {
+                        logger.info("Loading torrent from " + f.getName());
+                        tck.announce(TrackedTorrent.load(f));
+                    }
+
+                    logger.info("Starting tracker with {} announced torrents...",
+                            tck.getTrackedTorrents().size());
+                    tck.start();
+                    System.out.println(tck.getTrackedTorrents());
+                } catch (Exception e) {
+                    logger.error("{}", e.getMessage(), e);
+                    System.exit(2);
+                }
+            }
+
+            if(input.equals("info")){
+                System.out.println(t.getName());
+                System.out.println(t.getSize());
+                System.out.println(t.getAnnounceList());
+                System.out.println(t.getCreatedBy());
+            }
+        }
+
     }
 
+    private static Channel startClient(){
+        EventLoopGroup group = new NioEventLoopGroup();
+        Channel ch = null;
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                .channel(NioSocketChannel.class)
+                .handler(new TorrentClientInitializer());
+                // Make a new connection.
+                ch = b.connect("localhost", 5000).sync().channel();
+                // Get the handler instance to initiate the request.
+                //TorrentClientHandler handler = ch.pipeline().get(TorrentClientHandler.class);
+                // Request and get the response.
+                //List<String> response = handler.getLocalTimes(CITIES);
+                // Close the connection.
+                //sch.close();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+                //group.shutdownGracefully();
+        }
+
+        return ch;
+    }
+
+    private static Inet4Address getIPv4Address(String iface)
+            throws SocketException, UnsupportedAddressTypeException,
+            UnknownHostException {
+        if (iface != null) {
+            Enumeration<InetAddress> addresses =
+                    NetworkInterface.getByName(iface).getInetAddresses();
+            while (addresses.hasMoreElements()) {
+                InetAddress addr = addresses.nextElement();
+                if (addr instanceof Inet4Address) {
+                    return (Inet4Address)addr;
+                }
+            }
+        }
+
+        InetAddress localhost = InetAddress.getLocalHost();
+        if (localhost instanceof Inet4Address) {
+            return (Inet4Address)localhost;
+        }
+
+        throw new UnsupportedAddressTypeException();
+    }
 }
--- Client/src/main/java/Misc/TorrentUtil.java
+++ Client/src/main/java/Misc/TorrentUtil.java
@@ -1,4 +1,11 @@
 package Misc;
+import Network.TorrentWrapperOuterClass;
+import com.google.protobuf.ByteString;
+import com.turn.ttorrent.common.Torrent;
+import javafx.util.converter.ByteStringConverter;
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -9,47 +16,35 @@ import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
-import com.turn.ttorrent.common.Torrent;
-import org.apache.commons.io.IOUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class TorrentUtil {
 
     private static final Logger logger = LoggerFactory.getLogger(Torrent.class);
 
     /**
-     * Constructs a new util to handle
-     * and create torrent filel
-     *
-     * @param filename Name to save the torrent
+     * Creates a torrent file to upload
+     * @param trackers Name to save the torrent
      */
-
-    public TorrentUtil(String filename, String source)
-    {
+    public static Torrent createTorrent(String source, String username, ArrayList<String> trackers) throws IOException, NoSuchAlgorithmException {
+        return createTorrent(source, username, Torrent.DEFAULT_PIECE_LENGTH, trackers);
     }
 
-    public Torrent createTorrent(String filename, String source, int pieces) throws IOException, NoSuchAlgorithmException {
-        pieces = Torrent.DEFAULT_PIECE_LENGTH;
+    /**
+     * Creates a torrent file with specific pieces
+     * @param pieces Name to save the torrent
+     */
+    public static Torrent createTorrent(String source, String username, int pieces, ArrayList<String> trackers) throws IOException, NoSuchAlgorithmException {
+        //pieces = Torrent.DEFAULT_PIECE_LENGTH;
         //pieces = pieces * 1024;
         logger.info("Using piece length of {} bytes.", pieces);
-        logger.warn("At this point it should get the interested trackers");
+        Torrent torrent = null;
 
-        Vector<String> announceURLs = new Vector<String>();
-        announceURLs.add("http://localhost:6969");
-
-        OutputStream fos = null;
         try {
-            if (filename != null) {
-                fos = new FileOutputStream(filename);
-            }
-            //Process the announce URLs into URIs
             List<URI> announceURIs = new ArrayList<URI>();
-            for (String url : announceURLs) {
+            for (String url : trackers) {
                 announceURIs.add(new URI(url));
             }
-            //Create the announce-list as a list of lists of URIs
-            //Assume all the URI's are first tier trackers
+
             List<List<URI>> announceList = new ArrayList<List<URI>>();
             announceList.add(announceURIs);
 
@@ -60,31 +55,22 @@ public class TorrentUtil {
                                 sourceFile.getName());
             }
 
-            String creator = String.format("%s",
-                    System.getProperty("user.name"));
-
-            Torrent torrent = null;
             if (sourceFile.isDirectory()) {
                 logger.error("Can't transfer a directory.");
                 return null;
             } else {
-                torrent = Torrent.create(sourceFile, pieces, announceList, creator);
+                torrent = Torrent.create(sourceFile, pieces, announceList, username);
             }
-
-            torrent.save(fos);
 
         } catch (Exception e) {
             logger.error("{}", e.getMessage(), e);
             System.exit(2);
-        } finally {
-            if (fos != System.out) {
-                IOUtils.closeQuietly(fos);
-            }
-            return loadTorrent(filename);
         }
+
+        return torrent;
     }
 
-    public Torrent loadTorrent(String filename) throws IOException, NoSuchAlgorithmException
+    public static Torrent loadTorrent(String filename) throws IOException, NoSuchAlgorithmException
     {
         return Torrent.load(new File(filename), true);
     }
