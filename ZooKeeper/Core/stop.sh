352a2eb98afcb01cd4f3454cebba44f308fc8977
João Reis <johndivision@live.com.pt>
2018-04-24 16:00:28 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ ZooKeeper/Core/stop.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+zk-server-1/bin/zkServer.sh stop
+zk-server-2/bin/zkServer.sh stop
+zk-server-3/bin/zkServer.sh stop
--- frontServer/README.md
+++
@@ -1,9 +0,0 @@
-frontServer
-=====
-
-An OTP application
-
-Build
------
-
-    $ rebar3 compile
