84208d4b769f0b447a813eed19dda7a0297f06c9
markerstone <hugo.miguel.abreu@gmail.com>
2018-04-26 18:48:35 +0100

    Cleaned code and added required params

--- Client/src/main/java/Offline/Probes/Broadcast.java
+++ Client/src/main/java/Offline/Probes/Broadcast.java
@@ -1,7 +1,5 @@
 package Offline.Probes;
 
-//import protos.AddrOuterClass;
-
 import Network.AddrBroadcastOuterClass;
 import Offline.Utils.LocalAddresses;
 
@@ -27,90 +25,23 @@ public class Broadcast extends Thread {
         this.username = username;
         this.ips = ips;
         sockets = new HashMap<>();
-
         try{
-
             for(LocalAddresses ip : ips){
-
                 DatagramSocket ds = new DatagramSocket(new InetSocketAddress(ip.getIpv6(), sendPort));
                 ds.setBroadcast(true);
-
                 AddrBroadcastOuterClass.AddrBroadcast data = AddrBroadcastOuterClass.AddrBroadcast.newBuilder()
                         .setIpv6(ip.getIpv6())
                         .setIpv4(ip.getIpv4())
                         .setUsername(username).build();
-
                 ByteArrayOutputStream buff = new ByteArrayOutputStream(1024);
                         data.writeDelimitedTo(buff);
-
                 byte[] send = buff.toByteArray();
 
                 sockets.put(ds, send);
-
-
             }
-
-        }catch (SocketException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
-//        try {
-//            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
-//            while (interfaces.hasMoreElements()) {
-//                NetworkInterface iface = interfaces.nextElement();
-//                // filters out 127.0.0.1 and inactive interfaces
-//                if (iface.isLoopback() || !iface.isUp())
-//                    continue;
-//
-//                Enumeration<InetAddress> addresses = iface.getInetAddresses();
-//
-//                while(addresses.hasMoreElements()) {
-//                    InetAddress addr = addresses.nextElement();
-//                    if(Inet6Address.class == addr.getClass() && addr.isLinkLocalAddress()){
-//                        ips.add(addr.getHostAddress().replaceAll("%.*", "")); // e preciso tirar o %interface
-//                        ip = addr.getHostAddress().replaceAll("%.*", "");
-//                        System.out.println(ip);  //fe80:0:0:0:2936:f914:6634:5e05%wlp3s0
-//
-//
-//                        DatagramSocket ds = new DatagramSocket(new InetSocketAddress(ip, sendPort));
-//                        ds.setBroadcast(true);
-//
-//                        AddrOuterClass.Addr.Builder addrProto = AddrOuterClass.Addr.newBuilder();
-//                        addrProto.setAddr(ip);
-//                        addrProto.setPortNumber(5555);
-//                        AddrOuterClass.Addr data = addrProto.build();
-//
-//                        ByteArrayOutputStream buff = new ByteArrayOutputStream(1024);
-//                        data.writeDelimitedTo(buff);
-//
-//                        byte[] send = buff.toByteArray();
-//
-//                        sockets.put(ds, send);
-//
-////                        DatagramSocket ds = new DatagramSocket(port, InetAddress.getByName(ip));
-////                        ds.setBroadcast(true);
-////                        ds.setReuseAddress(true);
-////                        sockets.add(ds);
-////                        MulticastSocket ms = new MulticastSocket(new InetSocketAddress(ip,port));
-////                        sockets.add(ms);
-////                        System.out.println(ms.getInterface());
-//                        //a eduroam nao da ipv6 global so local
-//                        //se o ip comecar por fe80: e link local, ou seja, e so dentro da rede
-//                        //e preciso verificar se e global ou nao com isLinkLocalAddress()
-//                        //so deve haver 1 por cada interface
-//                        //se houverem varias interfaces depois vesse
-//                    }
-//                }
-//            }
-//        } catch (SocketException e) {
-//            throw new RuntimeException(e);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
     }
 
     public ArrayList<LocalAddresses> getIps() {
@@ -119,60 +50,18 @@ public class Broadcast extends Thread {
 
     @Override
     public void run(){
-
-        System.out.println("run");
-
+        System.out.println("Started broadcasting");
         try {
-
             while(true){
-
-//                for(DatagramSocket ds : sockets){
-//
-//                    AddrOuterClass.Addr.Builder addr = AddrOuterClass.Addr.newBuilder();
-//                    addr.setAddr(ds.getLocalAddress().toString());
-//                    addr.setPortNumber(5555);
-//                    AddrOuterClass.Addr data = addr.build();
-//
-//                    ByteArrayOutputStream buff = new ByteArrayOutputStream(1024);
-//                    data.writeDelimitedTo(buff);
-//
-//                    byte[] send = buff.toByteArray();
-//                    // ipv6 link local
-//                    DatagramPacket packet = new DatagramPacket(send, send.length, InetAddress.getByName("ff02::1"), port);
-//
-//                }
-
                 for (Map.Entry<DatagramSocket, byte[]> entry : sockets.entrySet()) {
-
                     DatagramPacket packet = new DatagramPacket(entry.getValue(), entry.getValue().length, InetAddress.getByName("ff02::1"), receivePort);
                     entry.getKey().send(packet);
-
                 }
-
-//                for(String to : ips){
-//                    // ipv6 link local
-//                    DatagramPacket packet = new DatagramPacket(send, send.length, InetAddress.getByName("ff02::1"), sendPort);
-//
-//                    //System.out.println(send.length);
-//
-//                    socket.send(packet);
-//                }
-
                 Thread.sleep(2000);
             }
 
-        } catch (SocketException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+        } catch (InterruptedException | IOException e) {
             e.printStackTrace();
         }
-
     }
-
-
-
 }
