6f92eb7a21a2e432bfa5b3895ec6279dcb8d7c6b
markerstone <hugo.miguel.abreu@gmail.com>
2018-04-26 12:41:02 +0100

    Added error handling for connection timeout

--- Client/src/main/java/Main.java
+++ Client/src/main/java/Main.java
@@ -1,14 +1,9 @@
-import Handlers.TorrentClientHandler;
-import Handlers.TorrentClientInitializer;
+import Handlers.TorrentListenerHandler;
+import Handlers.TorrentListenerInitializer;
 import Misc.TorrentUtil;
 import Network.TorrentWrapperOuterClass;
 import com.google.protobuf.ByteString;
-import com.turn.ttorrent.client.Client;
-import com.turn.ttorrent.client.SharedTorrent;
-import com.turn.ttorrent.client.peer.SharingPeer;
 import com.turn.ttorrent.common.Torrent;
-import com.turn.ttorrent.tracker.TrackedTorrent;
-import com.turn.ttorrent.tracker.Tracker;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.EventLoopGroup;
@@ -16,17 +11,13 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.Marker;
-import org.slf4j.MarkerFactory;
-
-import java.io.File;
-import java.io.FilenameFilter;
+import java.awt.*;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.*;
 import java.nio.channels.UnsupportedAddressTypeException;
 import java.security.NoSuchAlgorithmException;
 import java.util.*;
+import java.util.concurrent.TimeoutException;
 
 public class Main {
 
@@ -40,12 +31,14 @@ public class Main {
         Torrent t = null;
 
         Channel ch = startClient();
+        if(ch == null){
+            System.out.println("\u001B[31mError opening socket\u001B[0m");
+            System.exit(2);
+        }
         System.out.println("Started client");
 
         while (!(input = sc.nextLine()).equals("quit")){
-            if(input.equals("login")){
-                username = sc.nextLine();
-            }
+
             if(input.equals("upload")){
                 System.out.println("What is the file?");
                 ArrayList<String> trc = new ArrayList<String>();
@@ -63,67 +56,6 @@ public class Main {
                 System.out.println("Upload intention initiated");
             }
 
-            if(input.equals("download")){
-                System.out.println("What is the file?");
-                String file = sc.nextLine();
-                try {
-                    final SharedTorrent st = SharedTorrent.fromFile(
-                            new File(file),
-                            new File("/tmp/" + sc.nextLine()));
-
-                    Client c = new Client(
-                            getIPv4Address(null),
-                            st);
-
-                    c.setMaxDownloadRate(0.0);
-                    c.setMaxUploadRate(0.0);
-
-                    //Download and seed
-                    c.addObserver(new Observer() {
-                        @Override
-                        public void update(Observable o, Object arg) {
-                            System.out.println(st.getCompletion());
-                            System.out.println(arg);
-                        }
-                    });
-                    c.share(-1);
-
-                    if (Client.ClientState.ERROR.equals(c.getState())) {
-                        System.exit(1);
-                    }
-                } catch (Exception e) {
-                    logger.error("Fatal error: {}", e.getMessage(), e);
-                    System.exit(2);
-                }
-            }
-
-            if(input.equals("track")) {
-                FilenameFilter filter = new FilenameFilter() {
-                    @Override
-                    public boolean accept(File dir, String name) {
-                        return name.endsWith(".torrent");
-                    }
-                };
-
-                try {
-                    Tracker tck = new Tracker(new InetSocketAddress(6969));
-
-                    File parent = new File("./");
-                    for (File f : parent.listFiles(filter)) {
-                        logger.info("Loading torrent from " + f.getName());
-                        tck.announce(TrackedTorrent.load(f));
-                    }
-
-                    logger.info("Starting tracker with {} announced torrents...",
-                            tck.getTrackedTorrents().size());
-                    tck.start();
-                    System.out.println(tck.getTrackedTorrents());
-                } catch (Exception e) {
-                    logger.error("{}", e.getMessage(), e);
-                    System.exit(2);
-                }
-            }
-
             if(input.equals("info")){
                 System.out.println(t.getName());
                 System.out.println(t.getSize());
@@ -134,14 +66,14 @@ public class Main {
 
     }
 
-    private static Channel startClient(){
+    private static Channel startClient() throws SocketException, UnknownHostException {
         EventLoopGroup group = new NioEventLoopGroup();
         Channel ch = null;
         try {
             Bootstrap b = new Bootstrap();
             b.group(group)
                 .channel(NioSocketChannel.class)
-                .handler(new TorrentClientInitializer());
+                .handler(new TorrentListenerInitializer(getIPv4Address(null).getHostAddress()));
                 // Make a new connection.
                 ch = b.connect("192.168.43.243", 5000).sync().channel();
                 // Get the handler instance to initiate the request.
@@ -150,13 +82,12 @@ public class Main {
                 //List<String> response = handler.getLocalTimes(CITIES);
                 // Close the connection.
                 //sch.close();
-
-        } catch (InterruptedException e) {
+        } catch (Exception e) {
             e.printStackTrace();
+            return ch;
         } finally {
-                //group.shutdownGracefully();
+            group.shutdownGracefully();
         }
-
         return ch;
     }
 
