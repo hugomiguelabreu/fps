dc07eaaa97be8da1dfbf4c39ca00cac97a67705b
markerstone <hugo.miguel.abreu@gmail.com>
2018-05-08 12:44:59 +0100

    Changes Utils do delete files

--- Server/src/main/java/Util/TorrentUtil.java
+++ Server/src/main/java/Util/TorrentUtil.java
@@ -6,6 +6,7 @@ import com.turn.ttorrent.common.Peer;
 import com.turn.ttorrent.common.Torrent;
 import com.turn.ttorrent.tracker.TrackedPeer;
 import com.turn.ttorrent.tracker.TrackedTorrent;
+import com.turn.ttorrent.tracker.Tracker;
 import org.xml.sax.SAXException;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
@@ -34,7 +35,7 @@ public class TorrentUtil {
         return c;
     }
 
-    public static TrackedTorrent getTrackedTorrentWithObservers(Torrent t, Map<String, Client> clients) throws IOException, NoSuchAlgorithmException {
+    public static TrackedTorrent announceTrackedTorrentWithObservers(Tracker tck, Torrent t, Map<String, Client> clients) throws IOException, NoSuchAlgorithmException {
         TrackedTorrent tt = new TrackedTorrent(t);
 
         tt.addObserver((o, arg) -> {
@@ -43,11 +44,11 @@ public class TorrentUtil {
                 System.out.println("\u001B[31m" + tp.getIp() + " changed\u001B[0m");
                 if (!tp.getState().equals(TrackedPeer.PeerState.STOPPED) && !tp.getState().equals(TrackedPeer.PeerState.UNKNOWN)) {
                     if (tp.getLeft() == 0) {
-                        System.out.println("\u001B[31m" + tp.getIp() + " guy is over\u001B[0m");
+                        System.out.println("\u001B[31m" + tp.getIp() + " is over\u001B[0m");
 
                         for (TrackedPeer p : tt.getPeers().values()) {
                             if (p.getLeft() == 0)
-                                System.out.println("\u001B[31m" + p.getIp() + " other is over\u001B[0m");
+                                System.out.println("\u001B[31m" + p.getIp() + " is over\u001B[0m");
                         }
 
                         if (clients.containsKey(tt.getHexInfoHash()) &&
@@ -55,15 +56,25 @@ public class TorrentUtil {
                             System.out.println("\u001B[31mWe will remove local peer\u001B[0m");
                             clients.get(tt.getHexInfoHash()).stop();
                             clients.remove(tt.getHexInfoHash());
+
+                            /*tck.remove(tt);
+                            new Thread(() -> {
+                                try {
+                                    FileUtils.deleteFiles(tt);
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            });*/
+
                         }
 
                     } else {
-                        System.out.println("\u001B[31mNew guy, let's start a new local client\u001B[0m");
+                        System.out.println("\u001B[31mNew guy, let's start a new local client if we don't have one\u001B[0m");
                         if (!clients.containsKey(t.getHexInfoHash())) {
                             System.out.println("\u001B[31mWe don't have a client\u001B[0m");
                             Client c = null;
                             try {
-                                c = TorrentUtil.initClient(t, "/tmp/");
+                                c = TorrentUtil.initClient(t, FileUtils.fileDir);
                             } catch (IOException | NoSuchAlgorithmException | InterruptedException | ParserConfigurationException | SAXException e) {
                                 e.printStackTrace();
                             }
@@ -73,8 +84,9 @@ public class TorrentUtil {
                 }
             }catch(Exception e){
                     e.printStackTrace();
-                }
+            }
         });
+        tck.announce(tt);
         return tt;
     }
 }
