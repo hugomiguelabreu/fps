a42b6664389a9aefda02729ae2ed45344bc623d3
markerstone <hugo.miguel.abreu@gmail.com>
2018-05-10 17:24:47 +0100

    Added injection example to Handler

--- Server/src/main/java/Handlers/TorrentServerHandler.java
+++ Server/src/main/java/Handlers/TorrentServerHandler.java
@@ -3,12 +3,16 @@ package Handlers;
 import Util.FileUtils;
 import Util.TorrentUtil;
 import com.turn.ttorrent.client.Client;
+import com.turn.ttorrent.common.Peer;
 import com.turn.ttorrent.common.Torrent;
+import com.turn.ttorrent.tracker.TrackedPeer;
+import com.turn.ttorrent.tracker.TrackedTorrent;
 import com.turn.ttorrent.tracker.Tracker;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import server_network.ServerWrapper;
 
+import java.nio.ByteBuffer;
 import java.util.Map;
 
 public class TorrentServerHandler extends SimpleChannelInboundHandler<ServerWrapper.TrackerTorrent> {
@@ -31,7 +35,12 @@ public class TorrentServerHandler extends SimpleChannelInboundHandler<ServerWrap
         Client serverCli = TorrentUtil.initClient(t, FileUtils.fileDir);
         openClients.put(t.getHexInfoHash(), serverCli);
         //Get a tracked torrent with observables;
-        TorrentUtil.announceTrackedTorrentWithObservers(tck, t, openClients);
+        TrackedTorrent tt =  TorrentUtil.announceTrackedTorrentWithObservers(tck, t, openClients);
+        Peer cli = serverCli.getPeerSpec();
+        //PEERS SUPER NODOS
+        tt.injectPeer(new TrackedPeer(t, cli.getIp(), cli.getPort(), cli.getPeerId()));
+        tt.injectPeer(new TrackedPeer(t, "192.168.90.90", cli.getPort(), ByteBuffer.wrap("asdasd".getBytes(Torrent.BYTE_ENCODING))));
+        tt.injectPeer(new TrackedPeer(t, "192.168.90.91", cli.getPort(), ByteBuffer.wrap("asdasdasdasdasdas".getBytes(Torrent.BYTE_ENCODING))));
     }
 
     @Override
