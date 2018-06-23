6d67435ce83be435c7bbe8125204149c0cd64465
João Reis <johndivision@live.com.pt>
2018-06-23 10:50:49 +0100

    getgroups and getonline working; bugs fixed; changes to proto

--- front-server/zk/src/app2.erl
+++ front-server/zk/src/app2.erl
@@ -1,9 +1,9 @@
 -module(app2).
--export([start/1]).
+-export([start/0]).
 
 %falta tirar hard-code do ip ZK
-start(ID) ->
+start() ->
 	spawn(zk, init, ["localhost", 2184]),
 	spawn(data, init, []),
-	spawn(server_comm, init, [3001, ID]),
-	spawn(auth, init, [ID, 2001]).
+	spawn(server_comm, init, [3001, 2]),
+	spawn(auth, init, [2, 2001]).
