ab2196b44642092f93a6be1dd617eb838a32f4d6
João Reis <johndivision@live.com.pt>
2018-06-23 15:53:29 +0100

    Dynamic zookeeper connection

--- front-server/zk/src/app.erl
+++ front-server/zk/src/app.erl
@@ -3,7 +3,7 @@
 
 %falta tirar hard-code do ip ZK
 start(ID) ->
- 	spawn(zk, init, ["localhost", 2184]),
+ 	spawn(zk, init, ["localhost", 2182 + ID]),
 	timer:sleep(500),
 	spawn(data, init, []),
 	spawn(server_comm, init, [3000, ID]),
