2c352329608a3f4c5fc187e2e6c87d2bedad311a
markerstone <hugo.miguel.abreu@gmail.com>
2018-06-24 12:58:43 +0100

    WIP injected trackers

--- Server/src/main/java/Util/TorrentUtil.java
+++ Server/src/main/java/Util/TorrentUtil.java
@@ -51,11 +51,15 @@ public class TorrentUtil {
             if (!tp.getState().equals(TrackedPeer.PeerState.STOPPED) && !tp.getState().equals(TrackedPeer.PeerState.UNKNOWN)) {
                 if (tp.getLeft() == 0) {
                     System.out.println("\u001B[31m" + tp.getHexPeerId() + " is over\u001B[0m");
+                    if(tp.getHexPeerId().equals(clients.get(tt.getHexInfoHash()).getPeerSpec().getHexPeerId())){
+                        System.out.println("FUI EU QUE TERMINEI REMOVO TODOS OS INJETADOS");
+                        for(TrackedPeer injP: tt.getInjectedPeers())
+                            tt.removeInjectedPeer(injP.getHexPeerId());
+                    }
 
-                    //Se toda a gente terminou e todos os trackers também verificamos a eliminação.
+                    //Se toda a gente terminou e todos os trackers já pediram para terminar.
                     if ((clients.containsKey(tt.getHexInfoHash()) &&
-                            tt.getPeers().values().stream().allMatch(x -> x.getLeft() == 0)) &&
-                            tt.getInjectedPeers().stream().allMatch(x -> x.getLeft() == 0)) {
+                            tt.getPeers().values().stream().allMatch(x -> x.getLeft() == 0))) {
                         System.out.println("\u001B[31mWe will remove local peer\u001B[0m");
                         synchronized (clients) {
                             try {
